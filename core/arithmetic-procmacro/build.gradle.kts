7707510f57b2adc29b81a08df790995d8caaa087
Cyan <2802660864@qq.com>
2025-02-17 19:21:52 +0800

    feat:添加Rust模块.

---
+++ core/arithmetic-procmacro/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("multiplatform")
+    id("io.gitlab.trixnity.cargo.kotlin.multiplatform")
+    id("io.gitlab.trixnity.uniffi.kotlin.multiplatform")
+    alias(libs.plugins.kotlin.atomicfu)
+    alias(libs.plugins.android.library)
+}
+
+uniffi {
+    bindgenFromPath(rootProject.layout.projectDirectory.dir("bindgen"))
+    generateFromLibrary()
+}
+
+kotlin {
+    androidTarget()
+    jvm("desktop")
+
+    sourceSets {
+        commonTest {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(libs.kotest.assertions.core)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.fine.samples.arithmeticpm"
+    compileSdk = 34
+
+    defaultConfig {
+        consumerProguardFiles("proguard-rules.pro")
+        ndk.abiFilters.add("arm64-v8a")
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
